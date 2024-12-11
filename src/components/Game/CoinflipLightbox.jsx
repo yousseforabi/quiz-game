5bc2022df850a234e0617b139a35f3b59c85b2ae
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-12-11 07:31:47 +0100

    added svgs for coina

--- src/components/Game/CoinflipLightbox.jsx
+++ src/components/Game/CoinflipLightbox.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect } from "react";
 import "./CoinflipLightbox.css";
-import { use } from "react";
+import AngelSvg from "./angel-svg.svg";
+import SkullSvg from "./skull-svg.svg";
 
 
 function CoinflipLightbox(){
@@ -30,14 +31,15 @@ function CoinflipLightbox(){
 
     return(
         <div className="light-box">
-            <div className={`coin ${isFlipping ? "flip" : ""}`} onClick={handleFlip}>
+            <div className={`coin ${isFlipping ? "flip" : ""}`}>
                 <div 
-                className={`coin-face coin-heads ${flipResult === null ? "front" : flipResult === "Heads" ? "front" : "back"}`}
-                ></div>
-                <div className={`coin-face coin-tails ${flipResult === null ? "back" : flipResult === "Tails" ? "front" : "back"}`}>
+                className={`coin-heads ${flipResult === null ? "front" : flipResult === "Heads" ? "front" : "back"}`}
+                ><img className="angel-svg" src={AngelSvg} ></img></div>
+                <div className={`coin-tails ${flipResult === null ? "back" : flipResult === "Tails" ? "front" : "back"}`}
+                > <img className="skull-svg" src = {SkullSvg}></img>
                 </div>
             </div>
-            <h1>{flipResult && (userChoice === flipResult ?"You won" : "You lost")}</h1>
+            <h1 style={{color:"white"}}>{flipResult && (userChoice === flipResult ?"You won" : "You lost")}</h1>
         <button onClick={() => {handleFlip(), setUserChoice("Heads")}}>Heads</button>
         <button onClick={() => {handleFlip(), setUserChoice("Tails")}}>Tails</button>
         </div>
