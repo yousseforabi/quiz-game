b1aeb4f849e75f59955e596ccab29f74678d09a8
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-12-11 07:14:17 +0100

    adjusted timeout delay when setting result and reseting the reult value when running the block again

--- src/components/Game/CoinflipLightbox.jsx
+++ src/components/Game/CoinflipLightbox.jsx
@@ -1,11 +1,13 @@
 import { useState,useEffect } from "react";
 import "./CoinflipLightbox.css";
+import { use } from "react";
 
 
 function CoinflipLightbox(){
     
     const [isFlipping,setIsFlipping] = useState(false);
     const [flipResult,setFlipResult] = useState(null);
+    const [userChoice,setUserChoice] = useState(null);
     useEffect(() => {
     if(flipResult === null)return
     console.log(flipResult)
@@ -14,14 +16,14 @@ function CoinflipLightbox(){
 
     const handleFlip = () => {
         setIsFlipping(true);
+        setFlipResult(null)
         setTimeout(() => {
             const randomFlipResult = Math.random() < 0.5 ? "Heads" : "Tails";
             setFlipResult(randomFlipResult);
-        },1500)
+        },3500)
 
         setTimeout(() => {
-            setIsFlipping(false);
-            
+          setIsFlipping(false);
         },4000)
     }
 
@@ -31,10 +33,13 @@ function CoinflipLightbox(){
             <div className={`coin ${isFlipping ? "flip" : ""}`} onClick={handleFlip}>
                 <div 
                 className={`coin-face coin-heads ${flipResult === null ? "front" : flipResult === "Heads" ? "front" : "back"}`}
-                >Heads</div>
+                ></div>
                 <div className={`coin-face coin-tails ${flipResult === null ? "back" : flipResult === "Tails" ? "front" : "back"}`}>
-                Tails</div>
+                </div>
             </div>
+            <h1>{flipResult && (userChoice === flipResult ?"You won" : "You lost")}</h1>
+        <button onClick={() => {handleFlip(), setUserChoice("Heads")}}>Heads</button>
+        <button onClick={() => {handleFlip(), setUserChoice("Tails")}}>Tails</button>
         </div>
     )
 }
