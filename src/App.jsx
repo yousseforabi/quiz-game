a3e89ff2cb229c4b482e33a24e2a42e337fe12f2
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-12-17 00:28:23 +0100

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Game from "./components/Game/Game";
 import About from "./components/Auth/About";
+import Footer from "./components/Footer";
 
 function HomePage() {
   const { user } = useContext(UserContext);
@@ -39,21 +40,23 @@ function HomePage() {
 
 function App() {
   return (
-    <UserProvider >
+    <UserProvider>
       <Router>
         <nav>
-          <Link to="/">Home</Link> {" "} <Link to="/login">Login</Link> {" "}
-          <Link to="/register">Register</Link> {" "}  <Link to="/game">Game</Link>  {" "}
+          <Link to="/">Home</Link> {" "} 
+          <Link to="/login">Login</Link> {" "} 
+          <Link to="/register">Register</Link> {" "}  
+          <Link to="/game">Game</Link>  {" "} 
           <Link to="/about">About</Link> 
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<h1 className="start">Welcome to the Game!</h1>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/game" element={<Game />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <Footer path="/footer" /> 
       </Router>
     </UserProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 AnswerMe Quiz. All rights reserved.</p>
+      <p>
+        Follow us on:
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          Twitter
+        </a>{" "}
+        |{" "}
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          Facebook
+        </a>{" "}
+        |{" "}
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          Instagram
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
